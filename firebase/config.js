cecaa4c283e85efba87ad8cecc5d44a82f28327b
Joseph Jordan <jumpman.fam314@gmail.com>
2020-06-03 21:54:40 -0500

    added firebase

---
+++ firebase/config.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: "AIzaSyDhpf7FRYN_feS_pYHKGDBUuWpMAeK9Unw",
+  authDomain: "practice-firebase-projec-a586e.firebaseapp.com",
+  databaseURL: "https://practice-firebase-projec-a586e.firebaseio.com",
+  projectId: "practice-firebase-projec-a586e",
+  storageBucket: "practice-firebase-projec-a586e.appspot.com",
+  messagingSenderId: "65127967195",
+  appId: "1:65127967195:web:7c8e4034c0f350e37cdffe",
+  measurementId: "G-V04BZ1N9MT"
+};
